aac78935bb15d29eb2590796292b50181ee66467
Patrickadz9 <patrickadz9@gmail.com>
2022-09-13 08:10:05 -0700

    a program that prints the lowercase alphabet in reverse, followed by a new line.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,10 +11,12 @@
 int main(void) {
 	char low;
 
-	for (low = 'a'; low <= 'z'; low++) {
+	for (low = 'a'; low <= 'z'; low++)
+       	{
 		putchar(low);
-		putchar('\n');
 	}
+		putchar('\n');
+
 
 	return (0);
 }
